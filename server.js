d388ef2129bc68f06b20e784ed3625857937f192
Xinyue Li <xinyuel7@illinois.edu>
2020-05-11 22:58:39 -0500

    I'm so tired

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express=require('express');
 const graphqlHTTP=require('express-graphql');
-const schema = require('./schema')
+const cors = require('cors');
+const schema = require('./schema');
+const path = require('path');
 
 const app = express();
+
+//Allow cross-origin
+app.use(cors());
+
+
 app.use('/graphql', graphqlHTTP({ //only one endpoint
       schema,
       graphiql: true,
     }));
-  
+    
+app.use(express.static('public'));//public is our static folder
+app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, ()=>{
     console.log(`Server started on port ${PORT}`);
